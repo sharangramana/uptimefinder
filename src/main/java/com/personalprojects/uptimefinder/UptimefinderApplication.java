e9fe01206a47551a11619a56dad049da4dc81853
Sharang Ramana <sharangramana.g@kobil.com>
2022-11-29 18:34:01 +0530

    basic setup

---
+++ src/main/java/com/personalprojects/uptimefinder/UptimefinderApplication.java
@@ -0,0 +1,13 @@
+package com.personalprojects.uptimefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimefinderApplication.class, args);
+	}
+
+}
